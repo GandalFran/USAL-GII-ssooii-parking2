561d94708faeca485b8f16aaeb31617eb89973fb
Gabino Luis <gabinoluis@hotmail.es>
2018-04-26 21:15:46 +0200

    Small changes, debe de haber un problema de bucle infinito si sacamos toda la creacion de hilos fuera del bucle en la funcion de llegada. Ahora crea todo dentro del bucle si ya sabemos que la posicion es buena. En el futuro se puede hacer que desde ahi se llame a una funcion de creacion abstracta

--- parking2/parking2.cpp
+++ parking2/parking2.cpp
@@ -17,12 +17,15 @@ typedef int(*PARKING_Inicio)(TIPO_FUNCION_LLEGADA *, TIPO_FUNCION_SALIDA *, long
 typedef int(*PARKING_Aparcar)(HCoche, void *datos, TIPO_FUNCION_APARCAR_COMMIT,
 												   TIPO_FUNCION_PERMISO_AVANCE,
 												   TIPO_FUNCION_PERMISO_AVANCE_COMMIT);
+typedef int (*PARKING_Desaparcar)(HCoche, void *datos,
+										  TIPO_FUNCION_PERMISO_AVANCE,
+										  TIPO_FUNCION_PERMISO_AVANCE_COMMIT);
 typedef int(*PARKING_Get)(HCoche);
 typedef void*(*PARKING_GetDatos)(HCoche);
 
 typedef HCoche* PHCoche;
 typedef HANDLE CARRETERA, *PCARRETERA;
-typedef BOOL ACERA, *PACERA;
+//typedef BOOL ACERA, *PACERA;
 typedef struct _DatosCoche {
 	HCoche hc;
 	PHANDLE MutexOrden;
@@ -31,7 +34,7 @@ typedef struct _DatosCoche {
 HMODULE WINAPI ParkingLibrary;
 HCoche SiguienteCoche[4] = { 0, 0, 0, 0 };
 PCARRETERA Carretera[4];
-PACERA Acera[4];
+PBOOL Acera[4];
 PARKING_Get FuncionesGet[8];
 PARKING_Aparcar FuncionAparcar;
 PARKING_Inicio FuncionInicio;
@@ -40,18 +43,19 @@ PARKING_GetDatos FuncionGetDatos;
 void AparcarCommit(HCoche hc) {
 	PARKING_Get GetAlgoritmo = (PARKING_Get)GetProcAddress(ParkingLibrary, "PARKING2_getAlgoritmo");
 	if (GetAlgoritmo == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		exit(1);
 	}
 	PARKING_GetDatos GetDatos = (PARKING_GetDatos)GetProcAddress(ParkingLibrary, "PARKING2_getDatos");
 	if (GetDatos == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		exit(1);
 	}
 
 	SiguienteCoche[GetAlgoritmo(hc)] = hc;
 	PDATOSCOCHE DatosCoche = (PDATOSCOCHE)GetDatos(hc);
 	ReleaseMutex(*DatosCoche->MutexOrden);
+	fprintf(stderr, "[%d]Soy %d y escribo en la variable global y release mi mutex %d", __LINE__, hc, *DatosCoche->MutexOrden);
 }
 void PermisoAvance(HCoche hc) {}
 void PermisoAvanceCommit(HCoche hc) {}
@@ -62,18 +66,18 @@ DWORD WINAPI ChoferRoutine(LPVOID lpParam) {
 
 	PARKING_Get GetNumero = (PARKING_Get)GetProcAddress(ParkingLibrary, "PARKING2_getNUmero");
 	if (GetNumero == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		return 1;
 	}
 	PARKING_Get GetAlgoritmo = (PARKING_Get)GetProcAddress(ParkingLibrary, "PARKING2_getAlgoritmo");
 	if (GetAlgoritmo == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		return 1;
 	}
 
 	PARKING_GetDatos GetDatos = (PARKING_GetDatos)GetProcAddress(ParkingLibrary, "PARKING2_getDatos");
 	if (GetDatos == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		return 1;
 	}
 
@@ -82,16 +86,18 @@ DWORD WINAPI ChoferRoutine(LPVOID lpParam) {
 
 		PDATOSCOCHE DatosAnterior = (PDATOSCOCHE)GetDatos(CocheAnterior);
 
+		fprintf(stderr, "[%d] Espero por el mutex de %d que es %d", __LINE__, CocheAnterior, *DatosAnterior->MutexOrden);
 		DWORD WINAPI Value = WaitForSingleObject(*DatosAnterior->MutexOrden, INFINITE);
 		if (Value == WAIT_FAILED) {
-			fprintf(stderr, "Error al hacer wait sobre el mutex del coche anterior. Abortando... %d", GetLastError());
+			fprintf(stderr, "[%d]wait failed %d", __LINE__, GetLastError());
 			return 1;
 		}
+		fprintf(stderr, "[%d] He entrado en el mutex de %d que es %d", __LINE__, CocheAnterior, *DatosAnterior->MutexOrden);
 	}
 
 	PARKING_Aparcar Aparcar = (PARKING_Aparcar)GetProcAddress(ParkingLibrary, "PARKING2_aparcar");
 	if (Aparcar == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		return 1;
 	}
 
@@ -104,17 +110,17 @@ int PrimerAjuste(HCoche hc)
 {
 	PARKING_Get GetLongitud = (PARKING_Get)GetProcAddress(ParkingLibrary, "PARKING2_getLongitud");
 	if (GetLongitud == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d",__LINE__, GetLastError());
 		exit(1);
 	}
 
 	PARKING_Get GetAlgoritmo = (PARKING_Get)GetProcAddress(ParkingLibrary, "PARKING2_getAlgoritmo");
 	if (GetAlgoritmo == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		exit(1);
 	}
 
-	PACERA AceraAlg = Acera[GetAlgoritmo(hc)];
+	PBOOL AceraAlg = Acera[GetAlgoritmo(hc)];
 	int longitud;
 	int posInicial, longLibre, i = -1;
 
@@ -122,34 +128,28 @@ int PrimerAjuste(HCoche hc)
 	while (i < MAX_LONG_ROAD) {
 		i++;
 		longLibre = 0;
-		fprintf(stderr, "\nAceraAlg[%d] = %d",i, AceraAlg[i]);
 		while (AceraAlg[i] == FALSE && i < MAX_LONG_ROAD) {
 			i++;
 			longLibre++;
 			if (longLibre == longitud) {
 				posInicial = i - longitud;
-				memset(AceraAlg + posInicial, TRUE, sizeof(ACERA) * longitud);
-				i = INFINITE;
+				memset(AceraAlg + posInicial, TRUE, sizeof(BOOL) * longitud);
+				
+				PHANDLE PMutexOrden = (PHANDLE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(HANDLE));
+				*PMutexOrden = CreateMutex(NULL, TRUE, NULL);
+
+				PDATOSCOCHE Datos = (PDATOSCOCHE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(DATOSCOCHE));
+				Datos->MutexOrden = PMutexOrden;
+				Datos->hc = hc;
+
+				HANDLE nuevoThread = CreateThread(NULL, 0, ChoferRoutine, Datos, 0, NULL);
+
+				return posInicial;
 			}
 		}
 	}
-	fprintf(stderr, "\n\nLong = %d\n\n", longitud);
-	fprintf(stderr, "\n\ni = %d\n\n", i);
-	if (i == INFINITE) {
-		PHANDLE PMutexOrden = (PHANDLE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(HANDLE));
-		*PMutexOrden = CreateMutex(NULL, TRUE, NULL);
-
-		PDATOSCOCHE Datos = (PDATOSCOCHE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(DATOSCOCHE));
-		Datos->MutexOrden = PMutexOrden;
-		Datos->hc = hc;
-
-		HANDLE nuevoThread = CreateThread(NULL, 0, ChoferRoutine, Datos, 0, NULL);
-
-		return posInicial;
-	}
-	else {
-		return -1;
-	}
+	
+	return -1;
 }
 
 int SiguienteAjuste(HCoche hc)
@@ -167,8 +167,26 @@ int PeorAjuste(HCoche hc)
 	return -2;
 }
 
+DWORD WINAPI DesaparcarRoutine(LPVOID lpParam)
+{
+	PDATOSCOCHE Datos = (PDATOSCOCHE)lpParam;
+
+	PARKING_Desaparcar Desaparcar = (PARKING_Desaparcar)GetProcAddress(ParkingLibrary, "PARKING2_desaparcar");
+	if (Desaparcar == NULL) {
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
+		return 1;
+	}	
+
+	Desaparcar(Datos->hc, Datos, PermisoAvance, PermisoAvanceCommit);
+	return 0;
+}
+
 int Desaparcar(HCoche hc)
 {
+	PDATOSCOCHE Datos = (PDATOSCOCHE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(DATOSCOCHE));
+	Datos->hc = hc;
+
+	HANDLE nuevoThread = CreateThread(NULL, 0, DesaparcarRoutine, Datos, 0, NULL);
 	return 0;
 }
 
@@ -195,11 +213,15 @@ int main(int argc, char** argv)
 	}
 
 	for (int i = PRIMER_AJUSTE; i <= PEOR_AJUSTE; i++) {
-		Acera[i] = (PACERA)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(ACERA) * MAX_LONG_ROAD);
+		Acera[i] = (PBOOL)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(BOOL) * MAX_LONG_ROAD);
 		Carretera[i] = (PCARRETERA)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(CARRETERA) * MAX_LONG_ROAD);
 		for (int j = 0; j < MAX_LONG_ROAD; j++) {
 			Carretera[i][j] = CreateMutex(NULL, FALSE, NULL);
 		}
+	}
+
+	for (int i = 0; i < MAX_LONG_ROAD; i++) {
+		fprintf(stderr, "AceraAlg[%d] = %d\n", i, Acera[0][i]);
 	}
 
 	TIPO_FUNCION_LLEGADA FuncionesLlegada[] = { PrimerAjuste, SiguienteAjuste, MejorAjuste, PeorAjuste };
@@ -207,7 +229,7 @@ int main(int argc, char** argv)
 
 	PARKING_Inicio init = (PARKING_Inicio)GetProcAddress(ParkingLibrary, "PARKING2_inicio");
 	if (init == NULL) {
-		fprintf(stderr, "No se ha cargado la funcion correctamente. Abortando... %d", GetLastError());
+		fprintf(stderr, "[%d]No se ha cargado la funcion correctamente. Abortando... %d", __LINE__, GetLastError());
 		return 1;
 	}
 	
